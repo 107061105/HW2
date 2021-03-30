e592ecfba2c30291ec112bc4cb2eca221caa5bf0
107061105 <yc95086@gmail.com>
2021-03-30 11:12:50 -0700

    add main.cpp

--- demo/main.cpp
+++ demo/main.cpp
@@ -14,7 +14,7 @@ Timer debounce;                  //define debounce timer
 Timer t0;
 
 Thread t;
-float ADCdata[224];
+float ADCdata[200];
 
 int flag = 0;
 int c = 1;
@@ -83,40 +83,40 @@ int main(void)
 
       while (1) {
             if ((c == 0) && flag) {
-                  for (float i = 0.0f; i < 3.0/3.3; i += 0.1f) {
+                  for (float i = 0.0f; i <= 0.9; i += 0.1f) {
                         aout = i;
-                        wait_us(181);
+                        wait_us(182);
                         if (j == 201 || j == 202) ADCdata[k++] = Ain;
                   } 
-                  for (float i = 3.0/3.3; i > 0.0f; i -= 0.009f) {
+                  for (float i = 0.9; i > 0.0f; i -= 0.01f) {
                         aout = i;
-                        wait_us(162);
+                        wait_us(182);
                         if (j == 201 || j == 202) ADCdata[k++] = Ain;                        
                   }
             } else if ((c == 1) && flag) {
-                  for (float i = 0.0f; i < 3.0/3.3; i += 0.1f) {
+                  for (float i = 0.0f; i <= 0.9; i += 0.1f) {
                         aout = i;
-                        wait_us(40);
+                        wait_us(85);
                         if (j == 201 || j == 202) ADCdata[k++] = Ain;
                   } 
-                  for (float i = 3.0/3.3; i > 0.0f; i -= 0.009f) {
+                  for (float i = 0.9; i > 0.0f; i -= 0.01f) {
                         aout = i;
-                        wait_us(90);
+                        wait_us(85);
                         if (j == 201 || j == 202) ADCdata[k++] = Ain;                        
                   }
             } else if ((c == 2) && flag) {
-                  for (float i = 0.0f; i < 3.0/3.3; i += 0.1f) {
+                  for (float i = 0.0f; i <= 0.9; i += 0.1f) {
                         aout = i;
-                        wait_us(2);
+                        wait_us(5);
                         if (j == 201 || j == 202) ADCdata[k++] = Ain;
                   } 
-                  for (float i = 3.0/3.3; i > 0.0f; i -= 0.009f) {
+                  for (float i = 0.9; i > 0.0f; i -= 0.01f) {
                         aout = i;
-                        wait_us(4);
+                        wait_us(5);
                         if (j == 201 || j == 202) ADCdata[k++] = Ain;                        
                   }
             }
-            if (j == 202 && flag) for (int k = 0; k < 224; k++) printf("%f\r\n", ADCdata[k]);
+            if (j == 202 && flag) for (int k = 0; k < 200; k++) printf("%f\r\n", ADCdata[k]);
             j++;
       }
 }
